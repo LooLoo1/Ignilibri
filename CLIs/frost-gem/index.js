c014f2a457e56556d85f712676c22239cceede5a
Vitalii Petriv <vitaliy.petriv.work@gmail.com>
2024-01-30 08:44:16 +0200

    build: Initializing the FrostGem CLI
    And add to package.jsons -> "author": "Vitalii Petriv",

---
+++ CLIs/frost-gem/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+// "fg": "./index.js"
+console.log(process.argv)
+console.log(process.cwd()) // That it!!!
---
+++ CLIs/frost-gem/src/index.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+// console.log(process.argv)
+console.log(process.cwd()) // That it!!!
