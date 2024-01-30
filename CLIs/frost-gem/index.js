655ff3a6ad738cd8179ab6b8292b048670b8af18
Vitalii Petriv <vitaliy.petriv.work@gmail.com>
2024-01-30 11:25:20 +0200

    build: Add templates for `FrostGem CLI`, `pnpm scripts` and simple `ReadMe.md`

--- CLIs/frost-gem/index.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-// "fg": "./index.js"
-console.log(process.argv)
-console.log(process.cwd()) // That it!!!
--- CLIs/frost-gem/src/index.ts
+++ CLIs/frost-gem/src/index.ts
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 
-// console.log(process.argv)
+console.log(process.argv)
 console.log(process.cwd()) // That it!!!
+
+//TODO - Later will be implement responsive `--help` base on size of terminal.
+// console.log('Terminal size: ' + process.stdout.columns + 'x' + process.stdout.rows);
